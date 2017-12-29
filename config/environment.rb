aa0a18c2d7c0271157190308d642130129971abb
Ubuntu <ubuntu@ip-172-31-2-170.us-west-2.compute.internal>
2017-12-29 11:17:08 +0000

    Fix ruby gem versions.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Creating tables and problems with primary key in Rails
+require File.expand_path('../../lib/patches/abstract_mysql_adapter', __FILE__)
+
 # Initialize the rails application
 Peatio::Application.initialize!
